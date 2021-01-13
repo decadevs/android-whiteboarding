85ca720ff4ada7af9bd3d852e74a1053bc978c01
darot <darotudeen@gmail.com>
2021-01-13 12:54:21 +0100

    removed all other unwanted files

--- whiteboarding/src/main/kotlin/algorithm/main.kt
+++ whiteboarding/src/main/kotlin/algorithm/main.kt
@@ -1,12 +1,12 @@
 package algorithm
 
-import algorithm.problemsolving.ProblemSolving
+
 import algorithm.problemsolving.WhiteBoarding
 
 fun main(){
     val a = arrayListOf(1, 2, 4, 5, 8, 9)
     val b = WhiteBoarding.bSearch(a, 9)
-    val res2 = ProblemSolving.letterShiftingTwo("abC", 2)
+
     println(b)
 
 }
